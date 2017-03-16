ab5c1929c49cf33c47266663f9f5971dc8bf361d
JayWebDevCom <jaiye.s@gmail.com>
2017-03-16 21:06:03 +0000

    Add Challenge 8 array Session 2

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,9 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(array)
+	new_arr = []
+	array.each_with_index { |e,i| (array[i] == array[i+1] && array[i+1] == array[i+2]) ? new_arr << 'yes' : new_arr << 'no' }
+	new_arr.include?('yes') ? true : false
+end

150a6738ddfa85d4fc9202115a052cb6ecfbc1ab
JayWebDevCom <jaiye.s@gmail.com>
2017-03-17 22:58:04 +0000

    Add Challenge 3 hashes Session 3

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,14 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(num)
+	hash = Hash.new
+		(1..num).to_a.each { |y|
+			hash[y] = (1..y).to_a.reject!{ |t| t.odd? } if y.odd? 
+		}
+	hash	
+end
+
+puts staircase(5)
+
+

f08b7df80637092172ac0f24fc84366e47146971
JayWebDevCom <jaiye.s@gmail.com>
2017-03-17 15:54:07 +0000

    Add Challenge 11 classes Session 2

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -18,3 +18,28 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+
+class BeerSong
+		
+	def initialize(number)
+		number < 0 ? @number = 0 : @number = number
+		number > 99 ? @number = 99 : @nothing = "nothing"
+	end
+		
+	def verbalise(n)
+array = ["zero",  "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", "twenty nine", "thirty", "thirty one", "thirty two", "thirty three", "thirty four", "thirty five", "thirty six", "thirty seven", "thirty eight", "thirty nine", "forty", "forty one", "forty two", "forty three", "forty four", "forty five", "forty six", "forty seven", "forty eight", "forty nine", "fifty", "fifty one", "fifty two", "fifty three", "fifty four", "fifty five", "fifty six", "fifty seven", "fifty eight", "fifty nine", "sixty", "sixty one", "sixty two", "sixty three", "sixty four", "sixty five", "sixty six", "sixty seven", "sixty eight", "sixty nine", "seventy", "seventy one", "seventy two", "seventy three", "seventy four", "seventy five", "seventy six", "seventy seven", "seventy eight", "seventy nine", "eighty", "eighty one", "eighty two", "eighty three", "eighty four", "eighty five", "eighty six", "eighty seven", "eighty eight", "eighty nine", "ninety", "ninety one", "ninety two", "ninety three", "ninety four", "ninety five", "ninety six", "ninety seven", "ninety eight", "ninety nine"]
+#array[n].split(' ').map!{ |t| t.capitalize }.join(' ')
+array[n].split(' ').join('-').capitalize
+  end
+  
+  	def print_song
+		(1..@number).to_a.reverse.each { |num|
+			num == 1 ? @bot = "bottle" : @bot = "bottles"
+			num == 1 ? @bot_2 = "bottles" : @bot_2 = "bottles"
+			num == 2 ? @bot_2 = "bottle" : @bot_2 = "bottles"
+			puts "#{verbalise(num)} #{@bot} of beer on the wall,\n#{verbalise(num)} #{@bot} of beer,\nTake one down, pass it around,\n#{verbalise(num-1)} #{@bot_2} of beer on the wall."
+		}
+	end
+
+end

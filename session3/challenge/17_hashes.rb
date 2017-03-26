3c742c3db75224d33ab7d8c54ac3023ead257f64
JayWebDevCom <jaiye.s@gmail.com>
2017-03-26 21:49:48 +0100

    Add challenge 17_hashes session 3

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -7,3 +7,24 @@
 #
 # print_list_in_reverse head   # >> "1\n2\n"
 
+# I needed a lot of help with this and still need to go over it more before fully understanding
+# I am not so clear on what exactly linked lists are
+
+def print_list_in_reverse(hash = {})
+  puts extract(hash).reverse
+end
+
+def extract(hash = {}, results =[])
+  hash.each { |k,v|
+    if v.is_a?(Hash)
+      extract(v, results)
+    else
+      results << v if v.nil? == false
+    end
+  }
+  results
+end
+
+head = {:data => 2, :next => head}
+
+print_list_in_reverse head
